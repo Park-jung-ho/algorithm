0eb9ea24f78b192f7ed2ccfb2e02b995641eb599
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2023-11-30 23:54:14 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+    int N;
+    cin >> N;
+    if (N%2==1)
+    {
+        cout << "CY";
+    }
+    else
+    {
+        cout << "SK";
+    }
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
