e374e4b8d54db1896415a005b38c8baa8afdd57f
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2023-12-03 23:52:09 +0900

    [Bronze IV] Title: Tren del Fin del Mundo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30876. Tren del Fin del Mundo/Tren del Fin del Mundo.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int a, b;
+    cin >> a >> b;
+    n--;
+    while (n--)
+    {
+        int c, d;
+        cin >> c >> d;
+        if (d < b)
+        {
+            a = c;
+            b = d;
+        }
+        
+    }
+    cout << a << " " << b;   
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
