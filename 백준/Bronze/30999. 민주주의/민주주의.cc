2a5e1a152366ae5ea06494628a4063cf79e7291a
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2023-12-30 23:25:46 +0900

    [Bronze IV] Title: 민주주의, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30999. 민주주의/민주주의.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#include "DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    string a;
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        int cnt = 0;
+        for (auto c : a)
+        {
+            if (c == 'O') cnt++;
+        }
+        if (cnt > m/2) res++;
+    }
+    cout << res;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
