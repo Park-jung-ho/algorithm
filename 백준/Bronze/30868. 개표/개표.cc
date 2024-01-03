3e78567190acca06aade5c9ac97ac3236245956c
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-01-03 23:46:02 +0900

    [Bronze IV] Title: 개표, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30868. 개표/개표.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+    int N;
+    cin >> N;
+    for (int i = 0; i < N / 5; i++)
+        cout << "++++ ";
+    for (int i = 0; i < N % 5; i++)
+        cout << '|';
+    cout << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
