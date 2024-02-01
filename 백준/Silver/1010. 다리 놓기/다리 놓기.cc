86d3c1580f045c24abd1da76d8abe29e1471d411
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-02-01 22:26:08 +0900

    [Silver V] Title: 다리 놓기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    ll res = 1;
+    int c = 1;
+    for (int i = m; i > m - n; i--)
+    {
+        res *= i;
+        res /= c++;
+    }
+    cout << res << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
