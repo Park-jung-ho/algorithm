2049ed2ee00f481943530e7dc601122985ca3434
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-02-05 23:10:07 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+    string a;
+    cin >> a;
+    int cnt = 0;
+    for (auto c : a)
+    {
+        if (c == 'a') cnt++;
+        if (c == 'e') cnt++;
+        if (c == 'i') cnt++;
+        if (c == 'o') cnt++;
+        if (c == 'u') cnt++;
+    }
+    cout << cnt;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
