2de465c2eaafca1f5b70d6ad6c74844d96ca56c8
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-02-09 23:23:03 +0900

    [Bronze IV] Title: 치즈버거 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#define f first
+#define s second
+#define mp make_pair
+#define pb push_back
+
+#ifndef ONLINE_JUDGE
+#include "DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << min(a,b+1) + min(a-1,b);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
