0278baa8375862dbff009fc79c9c030b5b001955
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-03-19 22:09:58 +0900

    [Bronze IV] Title: 和の判定 (Sum Checker), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31607. 和の判定 （Sum Checker）/和の判定 （Sum Checker）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a+b==c || b+c==a || c+a == b) cout << 1;
+    else cout << 0;
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
