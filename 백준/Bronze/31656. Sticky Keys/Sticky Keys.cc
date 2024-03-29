d824ea9ae6961bff96b9d82e52857a20b9e26b69
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-03-29 23:36:15 +0900

    [Bronze IV] Title: Sticky Keys, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31656. Sticky Keys/Sticky Keys.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+    string a;
+    getline(cin,a);
+    cout << a[0];
+    for (int i = 1; i < a.size(); i++)
+    {
+        if (a[i] != a[i-1]) cout << a[i];
+    }
+    
+    
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    solve();
+}
