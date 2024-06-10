0cb61430695d0cc994ba4f01bfa6aeb8790bdba1
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-06-10 23:55:23 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+	int t, a, b;
+	cin >> t >> a >> b;
+	cout << min(t,a/2+b);
+	
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	solve();
+}
