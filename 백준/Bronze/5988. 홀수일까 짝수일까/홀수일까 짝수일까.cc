8f5266c9a71d6caaf8e486dbd3e203823c1a8cd0
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-07-07 21:05:28 +0900

    [Bronze III] Title: 홀수일까 짝수일까, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/5988. 홀수일까 짝수일까/홀수일까 짝수일까.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+	string a;
+	cin >> a;
+	int v = a[a.size()-1] - '0';
+	if (v%2==0) cout << "even\n";
+	else cout << "odd\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+}
