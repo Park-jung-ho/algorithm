f2bdf03c49e96d6e8a6b04d7935b7305938d1d2e
Park-jung-ho <119598764+Park-jung-ho@users.noreply.github.com>
2024-07-29 00:04:37 +0900

    [Bronze III] Title: 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ip = pair<int, int>;
+using ld = long double;
+using lp = pair<ll, ll>;
+
+#define all(v) (v).begin(), (v).end()
+
+#ifndef ONLINE_JUDGE
+#include "C:\DEBUG.h"
+#else
+#define debug(x...)
+#endif
+
+void solve()
+{
+	int n;
+	cin >> n;
+	int cnt = 0;
+	while (n--)
+	{
+		int a;
+		cin >> a;
+		cnt += a;
+	}
+	cout << cnt << "\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t;
+	cin >> t;
+	while (t--)
+		solve();
+}
